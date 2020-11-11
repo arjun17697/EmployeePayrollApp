987443359fc124884e12ddf000dd4f937a9377fc
arjun17697 <arjun17697@gmail.com>
2020-11-11 20:54:43 +0530

    Added Start Date label

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -1,7 +1,6 @@
-<script type="text/javascript">
-    const salary = document.querySelector('#salary');
-    const output = document.querySelector(".salary-output");
+const salary = document.querySelector('#salary')
+const output = document.querySelector('.salary-output')
+output.textContent = salary.value;
+salary.addEventListener('input', function () {
     output.textContent = salary.value;
-    salary.addEventListener('input', function () {
-        output.textContent = salary.value})
-</script>
+});
